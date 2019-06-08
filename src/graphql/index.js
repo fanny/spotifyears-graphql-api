30ab496b79dc13b16864e987aceb9f742424921b
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-06-07 21:15:01 -0300

    Fix identation

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,14 +3,7 @@ import trackSchema from './track/schema'
 import artistResolvers from './artist/resolvers'
 import trackResolvers from './track/resolvers'
 
-const typeDefs = [
-    artistSchema,
-    trackSchema
-]
-
-const resolvers = [
-    artistResolvers,
-    trackResolvers
-]
+const typeDefs = [artistSchema, trackSchema]
+const resolvers = [artistResolvers, trackResolvers]
 
 export { typeDefs, resolvers }
